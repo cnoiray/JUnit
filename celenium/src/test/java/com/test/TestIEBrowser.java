5330cd1b80be44f8d8edb4bc06891bf52ef8561b
cnoiray <clement.noiray@soprasteria.com>
2018-04-05 10:39:33 +0200

    ajout test celenium

--- celenium/src/test/java/com/test/TestIEBrowser.java
+++ celenium/src/test/java/com/test/TestIEBrowser.java
@@ -3,10 +3,14 @@
  */
 package com.test;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
@@ -17,6 +21,7 @@ public class TestIEBrowser {
 
     static String driverPath = "lib/";
     public static WebDriver driver;
+    public String baseUrl = "http://www.google.com";
 
     @BeforeClass
     public static void setUp() {
@@ -29,10 +34,18 @@ public class TestIEBrowser {
 
     @Test
     public void testGooglePageTitleInIEBrowser() {
-        driver.navigate().to("http://www.google.com");
+        // lancement google
+        driver.navigate().to(baseUrl);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        // vérification de la page google
         final String strPageTitle = driver.getTitle();
         System.out.println("Page title: - " + strPageTitle);
         Assert.assertEquals("Google", strPageTitle);
+
+        // écrire recherche dans input lst-ib
+        driver.findElement(By.id("lst-ib")).sendKeys("bloup");
+        driver.findElement(By.id("lst-ib")).sendKeys(Keys.ENTER);
     }
 
     @AfterClass
